5b89bd254b8bb2794fc3b17be49bcc02c434b6a0
hc24abp <hc24abp@COM-LG2-08.herts.ac.uk>
2026-02-23 11:34:18 +0000

    initial commit

---
+++ version_control_lab/src/main/java/labwork/version_control_lab/Version_control_lab.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package labwork.version_control_lab;
+
+/**
+ *
+ * @author hc24abp
+ */
+public class Version_control_lab {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
