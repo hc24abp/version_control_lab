5264666e098cbdf7113b39b555a456eca0b97087
hc24abp <hc24abp@COM-LG2-08.herts.ac.uk>
2026-02-23 12:38:03 +0000

    i have add new method called Complex(real double)

--- version_control_lab/src/main/java/Complex.java
+++ version_control_lab/src/main/java/Complex.java
@@ -14,8 +14,12 @@ public class Complex {
     public Complex(double real, double imaginary){
         this.real = real;
         this.imaginary = imaginary;
-  
 }
+    public Complex (double real){
+        this.real = real;
+        this.imaginary = 0;     
+    }
+    
 
 
 
